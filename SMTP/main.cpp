6142968bb2d3b682cdeff4c9a2d5ffd4685729cc
JeinKim <wh73plash@gmail.com>
2022-05-30 04:34:52 +0900

    Initial Commit

---
+++ SMTP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SMTP
+//
+//  Created by 김제인 on 2022/05/30.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
